fc6ffbf5b58d9db3fe52dbdc1011fd369303c006
shangjingfan <876625909@qq.com>
2021-04-01 01:21:10 +0800

    deploy

---
+++ precache-manifest.ec345d46407f957d8b16b9c57083d065.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8246d734c8598b5be9d35c00fdbd7331",
+    "url": "/money-memory-vue-website/Thumbs.db"
+  },
+  {
+    "revision": "ee94297ea2e5d9155192",
+    "url": "/money-memory-vue-website/css/app.d5fa9b00.css"
+  },
+  {
+    "revision": "528dfa3833bbf18655a946dac3fd887d",
+    "url": "/money-memory-vue-website/index.html"
+  },
+  {
+    "revision": "ee94297ea2e5d9155192",
+    "url": "/money-memory-vue-website/js/app.9b34420b.js"
+  },
+  {
+    "revision": "604bf99643fa29d79478",
+    "url": "/money-memory-vue-website/js/chunk-vendors.a0a538d7.js"
+  },
+  {
+    "revision": "e4abb6b2fc6d2476d8c4ee808604fe86",
+    "url": "/money-memory-vue-website/manifest.json"
+  },
+  {
+    "revision": "ca4885e78cbf959ded2f48e7f465dac1",
+    "url": "/money-memory-vue-website/qrcode1.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-memory-vue-website/robots.txt"
+  }
+]);
